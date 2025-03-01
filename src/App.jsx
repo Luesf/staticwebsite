568d4899de03362418b799303f86569e04963845
Luis Espinoza <lui.esp.11@gmail.com>
2025-02-28 18:51:18 -0600

    3rd commit testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
+import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>TESTING TESTING TESTING</h1>
-      <h2>P.S. I love you</h2>
+      <h2><LocalFloristIcon /> P.S. I love you <FavoriteBorderIcon /></h2>
     </div>
   )
 }
