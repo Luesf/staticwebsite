3c2d9afafaef3bd1f26c4d759587bc1504f95ee0
Luis Espinoza <lui.esp.11@gmail.com>
2025-03-05 21:11:16 -0600

    3rd update to login test site

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
+import Switch from '@mui/joy/Switch';
 
 function InputArea() {
+    const [isRegistered, setIsRegistered] = useState(false);
+
     return (
         <form className="input-area">
-            <label>Welcome mi amorcito!</label>
+            <label className="intro">Welcome mi amorcito!</label>
             <Input className="input" placeholder="Username..." variant="soft"/>
             <Input className="input" placeholder="Password..." variant="soft"/>
-            <Button className="login-button">Login</Button>
-            <Button className="register-button">Register</Button>
+            {isRegistered && <Input className="input" placeholder="Please confirm your password..." variant="soft"/>}
+            <Button className="login-button">{isRegistered ? "Register" : "Login"}</Button>
+            <label> New user? <Switch onChange={(event) => setIsRegistered(event.target.checked)}/></label>
         </form>
     )
 
